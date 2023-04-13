da0e20402953d5fb02f7a43b371af1a72616860f
ielshekeil <53063191+ielshekeil@users.noreply.github.com>
2023-04-13 17:44:14 -0500

    Delete seedemu directory

--- seedemu/compiler/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Docker import Docker, DockerImage
-
-# Disable the compilers options
-from .DistributedDocker import DistributedDocker
-from .Graphviz import Graphviz
-from .GcpDistributedDocker import GcpDistributedDocker
--- seedemu/layers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Base import Base
-from .Routing import Routing, Router, RealWorldRouter
-from .Ebgp import Ebgp, PeerRelationship
-from .Ospf import Ospf
-from .Ibgp import Ibgp
-from .Dnssec import Dnssec
-from .Mpls import Mpls
